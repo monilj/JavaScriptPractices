19b2400a92b79218c9d1795f5c3068541f7f2ca4
monilj <moniljoshi786@gmail.com>
2021-01-05 09:01:01 +0530

    Fundamentals of JavaScript-2 | Practice Introduction to Functions

---
+++ FundamentalOfJS-2/Practice/FunctionPart-1.js
@@ -0,0 +1,12 @@
+// writing function defination
+function logger() {
+    console.log('I am teacher');
+}
+// calling or invoking function
+logger();
+
+function juiceMachine(apple, orange) {
+    const juice = `Juice with ${apple} apples and ${orange} oranges`
+    return juice;
+}
+console.log(juiceMachine(3, 5));
