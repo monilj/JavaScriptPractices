b4ad686438b5c8d754751d7cfc5067af1b0a75ed
monilj <moniljoshi786@gmail.com>
2021-01-06 08:29:24 +0530

    Fundamentals of JavaScript Part-2 | Practice Introduction to Objects

---
+++ FundamentalOfJS-2/Practice/Introduction_to_Objects.js
@@ -0,0 +1,15 @@
+// array 
+const info = [
+    'Mike',
+    29,
+    'IT Professional',
+    ['Sam', 'John']
+]
+
+// object 
+const mikeInfo = {
+    firstName: 'Mike',
+    age: 29,
+    job: 'IT Professional',
+    friends: ['Sam', 'John']
+}
