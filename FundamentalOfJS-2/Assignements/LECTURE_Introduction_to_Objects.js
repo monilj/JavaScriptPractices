16ce54bf890ae6a217e1bae06c5dbbd45c468cc8
monilj <moniljoshi786@gmail.com>
2021-01-08 11:32:56 +0530

    Fundamentals of JavaScript | Assignment Introduction to Objects

---
+++ FundamentalOfJS-2/Assignements/LECTURE_Introduction_to_Objects.js
@@ -0,0 +1,9 @@
+const myCountry =
+{
+    'country': 'India',
+    'capital': 'Delhi',
+    'language': 'Sanskrit',
+    'population': '1300000',
+    'neighbours': ['Srilanka', 'Maldives', 'Pakistan', 'Chaina', 'Bangladesh']
+}
+console.log(myCountry);
