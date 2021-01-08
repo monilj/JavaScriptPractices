cd625dbf409e3977465a684ec24df3d248948ddf
monilj <moniljoshi786@gmail.com>
2021-01-08 09:51:53 +0530

    Fundamentals of JavaScript Part-2 | Add some more methods to  Basic Array operations

--- FundamentalOfJS-2/Assignements/LECTURE_Basic_Array_Operations_Methods.js
+++ FundamentalOfJS-2/Assignements/LECTURE_Basic_Array_Operations_Methods.js
@@ -1,8 +1,23 @@
 
-const neighbours = ['Srilank', 'Maldives', 'Pakistan', 'Chaina', 'Bangladesh'];
+const neighbours = ['Srilanka', 'Maldives', 'Pakistan', 'Chaina', 'Bangladesh'];
 
 console.log(neighbours);
 
 neighbours.push('Utopia');
 console.log(neighbours);
 
+neighbours.pop();
+console.log(neighbours);
+
+// Check for the neighbouring country
+if (neighbours.indexOf('Germany') < 0) {
+    console.log('Probably not a central European country')
+}
+else {
+    console.log('Central European country');
+}
+
+//Find the index of some country change the neighbour
+const indexOfACountry = neighbours.indexOf('Maldives');
+neighbours[indexOfACountry] = 'Thailand';
+console.log(neighbours);
