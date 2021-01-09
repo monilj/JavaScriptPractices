c33ae681997e519d34e6f1f24f4b973e0c9a45a4
monilj <moniljoshi786@gmail.com>
2021-01-09 19:45:37 +0530

    Fundamentals of javaScript Part -2 | Practice iteration For loop and Break and Continue statement

--- FundamentalOfJS-2/Practice/Iteration_for_loop_And_Usage.js
+++ FundamentalOfJS-2/Practice/Iteration_for_loop_And_Usage.js
@@ -6,8 +6,33 @@ const info = [
 ]
 type_array = [];
 
-for (let i = 1; i < info.length; i++) {
+for (let i = 0; i < info.length; i++) {
     // console.log(info[i]);
     type_array[i] = (typeof info[i]);
+    // type_array.push(typeof info[i])
 }
 console.log(type_array)
+
+const years = [1992, 1991, 1982, 1998];
+const age = [];
+for (let birthyear = 0; birthyear < years.length; birthyear++) {
+    age.push(2020 - years[birthyear]);
+}
+
+// Continue statment
+for (let i = 0; i < info.length; i++) {
+    if (typeof info[i] !== 'string')
+        continue;
+    console.log(typeof info[i]);
+}
+
+
+// Break statment
+for (let i = 0; i < info.length; i++) {
+    if (typeof info[i] === 'number')
+        break;
+    console.log(typeof info[i]);
+
+
+}
+// console.log(type_array)
