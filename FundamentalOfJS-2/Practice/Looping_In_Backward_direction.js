218d15f919991e7e7c16fe016f77579800a19701
monilj <moniljoshi786@gmail.com>
2021-01-09 19:56:44 +0530

    Fundamentals of javaScript Part -2 | Practice Looping in Backward

---
+++ FundamentalOfJS-2/Practice/Looping_In_Backward_direction.js
@@ -0,0 +1,22 @@
+const info = [
+    'Mike',
+    29,
+    'IT Professional',
+    ['Sam', 'John']
+]
+type_array = [];
+
+//Loooping in backward direction 
+for (let i = info.length - 1; i >= 0; i--) {
+    type_array[i] = (typeof info[i]);
+}
+console.log(type_array);
+
+// Repetation 3 execercise for 5 time
+for (excercise = 1; excercise < 4; excercise++) {
+    console.log(`Starting excercise ${excercise}`);
+    for (let rep = 1; rep < 6; rep++) {
+        console.log(`lifting wait repitation ${rep}`);
+    }
+
+}
