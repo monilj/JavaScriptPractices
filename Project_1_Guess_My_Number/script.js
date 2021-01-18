ad8888e7e31e4c0b94b98c5f73f9e4593fbdf5f1
monilj <moniljoshi786@gmail.com>
2021-01-18 20:02:39 +0530

    Project 1 Guess My Number | Implementation Game Logic

--- Project_1_Guess_My_Number/script.js
+++ Project_1_Guess_My_Number/script.js
@@ -1,5 +1,5 @@
 'use strict';
-console.log(document.querySelector('.message').textContent);
+/* console.log(document.querySelector('.message').textContent);
 document.querySelector('.message').textContent = 'Correct Number ';
 console.log(document.querySelector('.message').textContent);
 
@@ -7,14 +7,39 @@ document.querySelector('.number').textContent = 13;
 document.querySelector('.score').textContent = 20;
 console.log(document.querySelector('.guess').value);
 document.querySelector('.guess').value = 23;
-console.log(document.querySelector('.guess').value);
+console.log(document.querySelector('.guess').value); */
 
 // Seperator
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+document.querySelector('.number').textContent = secretNumber;
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
   if (!guess) {
     document.querySelector('.message').textContent = 'Not a Number';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct Number!';
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too High';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too Low';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game';
+      document.querySelector('.score').textContent = 0;
+    }
   }
 });
