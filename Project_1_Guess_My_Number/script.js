9e10f8b6167c59c15c934d6ecdb9a5b141a6609f
monilj <moniljoshi786@gmail.com>
2021-01-18 12:55:14 +0530

    Project 1 Guess My Number | Handling Click Events

--- Project_1_Guess_My_Number/script.js
+++ Project_1_Guess_My_Number/script.js
@@ -8,3 +8,13 @@ document.querySelector('.score').textContent = 20;
 console.log(document.querySelector('.guess').value);
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
+
+// Seperator
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'Not a Number';
+  }
+});
