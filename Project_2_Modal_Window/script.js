807a7b197f81c4ea14af899fd5ab4847c7ba2b49
monilj <moniljoshi786@gmail.com>
2021-01-22 11:24:27 +0530

    Project 2 Modal Window | Refactore Code | Create seperate Function for show Modal and Close Modal | Declare those function in Event Listener

--- Project_2_Modal_Window/script.js
+++ Project_2_Modal_Window/script.js
@@ -4,19 +4,21 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnShowModal = document.querySelectorAll('.show-modal');
 
-for (let i = 0; i < btnShowModal.length; i++)
-  btnShowModal[i].addEventListener('click', function () {
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-  });
+const showModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
 
 const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
 };
+
+for (let i = 0; i < btnShowModal.length; i++)
+  btnShowModal[i].addEventListener('click', showModal);
 btnCloseModal.addEventListener('click', closeModal);
-// overlay.addEventListener('click', closeModal);
-overlay.addEventListener('click', function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
-});
+overlay.addEventListener('click', closeModal);
+// overlay.addEventListener('click', function () {
+//   modal.classList.add('hidden');
+//   overlay.classList.add('hidden');
+// });
