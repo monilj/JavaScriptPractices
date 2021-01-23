5908acb614f2ea2bc6d76fc9fdc5fc9435336d4a
monilj <moniljoshi786@gmail.com>
2021-01-23 15:36:51 +0530

    Project 3 Pig Game | Switching the Active Player

--- Project_3_Pig_Game/script.js
+++ Project_3_Pig_Game/script.js
@@ -9,13 +9,17 @@ const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 const current0El = document.getElementById('current--0');
 const current1El = document.getElementById('current--1');
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 
 // Set Scrores to 0 and  Hide dice image at initial phase
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
-
 let currentScore = 0;
+let activePlayer = 0;
+const score = [0, 0];
+
 btnRoll.addEventListener('click', function () {
   // Generating Random Dice roll
   const dice = Math.trunc(Math.random() * 6) + 1;
@@ -27,8 +31,15 @@ btnRoll.addEventListener('click', function () {
   if (dice !== 1) {
     //   Add dice to current Score
     currentScore += dice;
-    current0El.textContent = currentScore;
+    document.getElementById(
+      `current--${activePlayer}`
+    ).textContent = currentScore;
   } else {
     // switch player
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    currentScore = 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
   }
 });
