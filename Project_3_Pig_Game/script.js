dd8fd7dde2c3765d8fdf722cbf216bf6d4c41189
monilj <moniljoshi786@gmail.com>
2021-01-26 05:44:44 +0530

    Project 3 Pig Game | Refactoring Code

--- Project_3_Pig_Game/script.js
+++ Project_3_Pig_Game/script.js
@@ -3,24 +3,40 @@
 // Selecting Elements
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
-const diceEl = document.querySelector('.dice');
-const btnNew = document.querySelector('.btn--new');
-const btnRoll = document.querySelector('.btn--roll');
-const btnHold = document.querySelector('.btn--hold');
 const current0El = document.getElementById('current--0');
 const current1El = document.getElementById('current--1');
 const player0El = document.querySelector('.player--0');
 const player1El = document.querySelector('.player--1');
 
-// Set Scrores to 0 and  Hide dice image at initial phase
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
-let currentScore = 0;
-let activePlayer = 0;
-const score = [0, 0];
-let playing = true;
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
 
+let scores, currentScore, activePlayer, playing;
+// Set Scrores to 0 and  Hide dice image at initial phase
+
+const init = function () {
+  score[(0, 0)];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+  activePlayer = score0El.textContent = 0;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  diceEl.classList.add('hidden');
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+  document
+    .querySelector(`.player--${activePlayer}`)
+    .classList.remove('player--winner');
+  document.querySelector(`.player--0`).classList.add('player--active');
+  playing = true;
+};
+init();
+
+// init();
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
   activePlayer = activePlayer === 0 ? 1 : 0;
@@ -73,4 +89,18 @@ btnHold.addEventListener('click', function () {
       switchPlayer();
     }
   }
+});
+
+btnNew.addEventListener('click', function () {
+  score0El.textContent = 0;
+
+  score1El.textContent = 0;
+  diceEl.classList.add('hidden');
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+  document
+    .querySelector(`.player--${activePlayer}`)
+    .classList.remove('player--winner');
+  document.querySelector(`.player--0`).classList.add('player--active');
+  playing = true;
 });
