9153fd5ddd1fa7713d13768fdff294d41072ef78
monilj <moniljoshi786@gmail.com>
2021-03-08 23:16:45 +0530

    Data Structure & Modern Operators | Working with Strings Part -1

---
+++ Data_Structures-Moder_Operators-String/Working_with_Strings_Part-1.js
@@ -0,0 +1,33 @@
+'use strict';
+const airline = 'Tap air India';
+const plane = 'A320';
+console.log(plane[0]);
+console.log(plane[1]);
+console.log(plane[2]);
+console.log('B737'[0]);
+// we can use properties
+console.log('B737'.length);
+// we can use methods
+console.log('B737'.indexOf('B'));
+console.log('B737'.indexOf('7'));
+console.log('B737'.lastIndexOf('7'));
+
+console.log(airline.slice(4));
+console.log(airline.slice(4, 7));
+
+// identify index using data
+console.log(airline.slice(0, airline.indexOf(' ')));
+console.log(airline.slice(airline.lastIndexOf(' ') + 1));
+
+// negative argument
+console.log(airline.slice(-2));
+console.log(airline.slice(1, -1));
+const checkMiddleSeat = function (seat) {
+  const s = seat.slice(-1);
+  if (s === 'B' || s === 'E') {
+    console.log('You got middle seat');
+  } else console.log('You are lucjy');
+};
+checkMiddleSeat('11B');
+checkMiddleSeat('23C');
+checkMiddleSeat('3E');
