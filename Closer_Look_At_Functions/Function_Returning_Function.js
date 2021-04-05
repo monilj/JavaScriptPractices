0dc66b5b0c891468868848dd00317d556f84dd64
monilj <moniljoshi786@gmail.com>
2021-04-05 08:26:11 +0530

    Closer look at Functions

---
+++ Closer_Look_At_Functions/Function_Returning_Function.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const greet = function (greeting) {
+  return function (name) {
+    console.log(`${greeting} ${name}`);
+  };
+};
+
+const greeterHey = greet('Hey');
+greeterHey('John');
+
+greet('Hello')('John');
+
+const greetArr = greeting => name => console.log(`${greeting} ${name}`);
+greetArr('Hi')('Smith');
