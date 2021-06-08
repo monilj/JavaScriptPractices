9228bd7a77722b1701c1a2951c5d738bf22c1e7d
moniljoshi <monil.joshi@qualitia.com>
2021-06-08 22:34:05 +0530

    Closer Look at Function | Clousers

---
+++ Closer_Look_At_Functions/Closures.js
@@ -0,0 +1,12 @@
+'use strict';
+const secureBooking = function () {
+  let passengerCount = 0;
+  return function () {
+    passengerCount++;
+    console.log(`${passengerCount} passengers`);
+  };
+};
+const booker = secureBooking();
+booker();
+booker();
+booker();
