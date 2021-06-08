325882c1f531aa077247593a8411027045b8869c
moniljoshi <monil.joshi@qualitia.com>
2021-06-08 23:29:23 +0530

    Working with Arrays | For Each Method

---
+++ Working_With_Arrays/Logging_Arrays_ForEach.js
@@ -0,0 +1,20 @@
+'use strict';
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+// for (const movement of movements)
+for (const [i, movement] of movements.entries()) {
+  if (movement > 0) {
+    console.log(`Movement ${i + 1}: You deposited ${movement}`);
+  } else {
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(movement)}`);
+  }
+}
+
+// movements.forEach(function (movement) {
+movements.forEach(function (movement, i, array) {
+  if (movement > 0) {
+    console.log(`Movement ${i + 1}: You deposited ${movement}`);
+  } else {
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(movement)}`);
+  }
+});
